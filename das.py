686ef6b918518b4e2a0e49a42ce798a7ed17315a
paliy89 <pav892018@gmail.com>
2020-01-21 10:55:26 +0200

    Initial commit

---
+++ das.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+root = Tk()
+root.title("Калькулятор")
+
+
+#логіка калькулятора
+def calc(key):
+    global memory
+    if key == "=":
+#виключення написання букв
+        str1 = "-+0123456789.*/"
+        if calc_entry.get()[0] not in str1:
+            calc_entry.insert(END, "Перший символ не число!")
+            messagebox.showerror("Помилка!", "Ви ввели не число")
+
+#рахунок
+        try:
+            result = eval(calc_entry.get())
+            calc_entry.insert(END, "=" + str(result))
+        except:
+            calc_entry.insert(END, "Помилка!")
+            messagebox.showerror("Помилка!", "Провірь правельність даних")
+# очистити поле
+    elif key == "C":
+        calc_entry.delete(0, END)
+# зміна + на -
+    elif key == "-/+":
+        if "=" in calc_entry.get():
+            calc_entry.delete(0, END)
+        try:
+            if calc_entry.get()[0] == "-":
+                calc_entry.delete(0)
+            else:
+                calc_entry.insert(0, "-")
+        except IndexError:
+            pass
+    else:
+        if "=" in calc_entry.get():
+            calc_entry.delete(0, END)
+        calc_entry.insert(END, key)
+# Створюємо кнопки
+bttn_list = [
+    "7", "8", "9", "+", "-",
+    "4", "5", "6", "*", "/",
+    "1", "2", "3", "-/+", "=",
+    "0", ".", "C", "", ""
+]
+r = 1
+c = 0
+
+for i in bttn_list:
+    rel = ""
+    cmd = lambda x = i: calc(x)
+    ttk.Button(root, text=i, command=cmd).grid(row=r, column=c)
+    c += 1
+    if c > 4:
+        c = 0
+        r += 1
+
+calc_entry = Entry(root, width=33)
+calc_entry.grid(row=0, column=0, columnspan=5)
+
+root.mainloop()
